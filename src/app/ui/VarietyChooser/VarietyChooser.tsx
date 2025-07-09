19a6e73a7b590423a1057df044b61aa6d8d0f541
Simon Cornelius <simonjoecornelius@gmail.com>
2025-07-09 17:23:36 +0200

    feat: add search for varieties
    also makes the component twice as wide

--- src/app/ui/VarietyChooser/VarietyChooser.tsx
+++ src/app/ui/VarietyChooser/VarietyChooser.tsx
@@ -1,4 +1,5 @@
 import { VarietyType } from "@/types/variety";
+import { useMemo, useState } from "react";
 
 type VarietyChooserProps = {
     availableVarieties: VarietyType[];
@@ -7,23 +8,39 @@ type VarietyChooserProps = {
 }
 
 export default function VarietyChooser({ availableVarieties, chosenVarieties, chooseVariety }: VarietyChooserProps) {
+    const [search, setSearch] = useState<string>("");
     const varietiesWithIsChosen: [VarietyType, boolean][] = availableVarieties
-    .map<[VarietyType, boolean]>((variety) => [variety, chosenVarieties.some((v) => v.id === variety.id)])
-    .sort((a, b) => a[0].name.localeCompare(b[0].name)); // need to re-sort array afterwards
+        .map<[VarietyType, boolean]>((variety) => [variety, chosenVarieties.some((v) => v.id === variety.id)])
+        .sort((a, b) => a[0].name.localeCompare(b[0].name)); // need to re-sort array afterwards
+    
+    const filteredVarieties = useMemo(() =>
+        varietiesWithIsChosen.filter(([vp]) =>
+            vp.name.toLowerCase().includes(search.toLowerCase())
+        ), [search, varietiesWithIsChosen]
+    );
 
     return (
-        <ul className="bg-gray-100 rounded-sm p-2">
-            {varietiesWithIsChosen.map(([variety, isChosen]) => (
-                <li key={variety.id} onClick={() => {
-                    if (!isChosen) chooseVariety(variety);
-                }} className={`
-                p-1 text-sm
-                ${isChosen ?
-                    'bg-gray-200 text-gray-400 disabled'
-                    : 'hover:bg-[var(--color-primary-muted)] cursor-pointer'
-                }
-                `}>{variety.name}</li>
-            ))}
-        </ul>
+        <div>
+            <input
+            type="text"
+            placeholder="Suche nach Rebsorten..."
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+            className="w-full px-3 py-2 border-b border-gray-300 text-sm focus:outline-none focus:border-[var(--color-primary)]"
+            />
+            <ul className="bg-gray-100 rounded-sm p-2">
+                {filteredVarieties.map(([variety, isChosen]) => (
+                    <li key={variety.id} onClick={() => {
+                        if (!isChosen) chooseVariety(variety);
+                    }} className={`
+                    p-1 text-sm
+                    ${isChosen ?
+                        'bg-gray-200 text-gray-400 disabled'
+                        : 'hover:bg-[var(--color-primary-muted)] cursor-pointer'
+                    }
+                    `}>{variety.name}</li>
+                ))}
+            </ul>
+        </div>
     )
 }
